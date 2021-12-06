67751cafe88450d3602ae36eec40df1d37f53830
nihalerdal <nihalerdall@gmail.com>
2021-12-06 15:03:22 -0800

    update

---
+++ FDARecalls/Model/Data/ProductModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProductModel.swift
+//  FDARecalls
+//
+//  Created by Nihal Erdal on 12/6/21.
+//
+
+import Foundation
+
+struct RecalledProduct {
+    
+    static var address: String = ""
+}
--- FDARecalls/Response/GetRecalls.swift
+++ FDARecalls/Response/GetRecalls.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct GetRecalls: Codable {
-   
+    
     let firmlegalnam: String
     let firmcitynam: String
     let firmcountrynam: String
@@ -23,5 +23,22 @@ struct GetRecalls: Codable {
     let productdistributedquantity: String
     let phasetxt : String
     let centercd : String
-     
+    
+    init(firmlegalnam: String, firmcitynam: String, firmcountrynam: String, firmline1adr: String,firmline2adr:String, firmpostalcd: String, productid:String, productdescriptiontxt:String, productshortreasontxt:String, createdt:Date, centerclassificationtypetxt: String, productdistributedquantity: String, phasetxt:String, centercd:String   ) {
+        
+        self.firmlegalnam = firmlegalnam
+        self.firmcitynam = firmcitynam
+        self.firmcountrynam = firmcountrynam
+        self.firmline1adr = firmline1adr
+        self.firmline2adr = firmline2adr
+        self.productid = productid
+        self.productdescriptiontxt = productdescriptiontxt
+        self.productshortreasontxt = productshortreasontxt
+        self.createdt = createdt
+        self.centerclassificationtypetxt = centerclassificationtypetxt
+        self.productdistributedquantity = productdistributedquantity
+        self.phasetxt = phasetxt
+        self.centercd = centercd
+    }
+    
 }

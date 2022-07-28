9ecd312a44b7ee4134212501adf50685fea82e8c
nihalerdal <nihalerdall@gmail.com>
2022-07-28 16:20:28 -0700

    orginazed folders

--- FDARecalls/ViewController/DetailReportVC.swift
+++ FDARecalls/ViewController/DetailReportVC.swift
@@ -23,18 +23,9 @@ class DetailReportVC: UIViewController {
         reasonTextView.text = myProduct.recallReason
         productLabel.text = "Product"
         reasonLabel.text = "Reason Of Recall"
+        productTextView.layer.cornerRadius = 10
+        reasonTextView.layer.cornerRadius = 10
     }
-    
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
